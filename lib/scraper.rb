e2dad303421e8e42ed77db4243e8f06de7693c26
Sonja Parsell <hello@sparsell.com>
2020-05-18 10:41:32 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,4 +4,5 @@ require 'open-uri'
 html = open("https://flatironschool.com/")
 
 doc = Nokogiri::HTML(html)
-p doc.css(".headline-26OIBN").text.strip
+#p doc.css(".headline-26OIBN").text.strip
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
